7dd3f434a8983fc7bf52162eeb340438eddc50a2
tnanhd <tnanh.dev@gmail.com>
2024-08-28 20:57:10 +0700

    Feat: Draw LoadingButton with custom attr for background color

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,11 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -12,6 +15,7 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+    private var backgroundColor = 0
 
     private val valueAnimator = ValueAnimator()
 
@@ -19,15 +23,27 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
-
-    init {
-
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create("", Typeface.BOLD)
     }
 
+    init {
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            backgroundColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
+        paint.color = backgroundColor
+        canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
