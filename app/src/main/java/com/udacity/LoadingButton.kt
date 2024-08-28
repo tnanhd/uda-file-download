c8a4e8450d6a6d2f5ba544947251dd82fc703350
tnanhd <tnanh.dev@gmail.com>
2024-08-28 22:18:58 +0700

    Feat: Draw LoadingButton's text with custom attrs

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -4,6 +4,8 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.Rect
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
@@ -15,7 +17,16 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+
+    // Custom attributes
     private var backgroundColor = 0
+    private var textColor = 0
+    private var textSize = 0.0f
+    private var text = ""
+
+    private val screenHeight = context.resources.displayMetrics.heightPixels
+
+    private val textPosition: PointF = PointF(0.0f, 0.0f)
 
     private val valueAnimator = ValueAnimator()
 
@@ -33,17 +44,21 @@ class LoadingButton @JvmOverloads constructor(
     init {
         context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
             backgroundColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
+            textColor = getColor(R.styleable.LoadingButton_textColor, 0)
+            textSize = convertSpToPixels(getString(R.styleable.LoadingButton_textSize) ?: "0sp")
+            text = getString(R.styleable.LoadingButton_text) ?: ""
         }
-    }
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         paint.color = backgroundColor
-        canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+
+        paint.color = textColor
+        paint.textSize = textSize
+        textPosition.computeXYForText()
+        canvas?.drawText(text, textPosition.x, textPosition.y, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -59,4 +74,26 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+    private fun PointF.computeXYForText() {
+        val bounds = Rect()
+        paint.getTextBounds(text, 0, text.length, bounds)
+        val textHeight = bounds.height()
+
+        x = (widthSize / 2).toFloat()
+        y = ((heightSize + textHeight) / 2).toFloat()
+    }
+
+    private fun convertSpToPixels(sp: String): Float {
+        val modifiedString = if (sp.length > 2) {
+            sp.substring(0, sp.length - 2)
+        } else {
+            sp
+        }
+        return try {
+            modifiedString.toFloat() * context.resources.displayMetrics.scaledDensity
+        } catch (e : NumberFormatException) {
+            0.0f
+        }
+
+    }
 }
